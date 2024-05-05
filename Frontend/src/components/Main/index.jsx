6d0f2bb3d97c5e66f7cdcb8d632f0ff3824122e8
Sri Harish <sriharish7635@gmail.com>
2024-05-05 09:05:06 +0530

    upload functionality

--- Frontend/src/components/Main/index.jsx
+++ Frontend/src/components/Main/index.jsx
@@ -1,4 +1,5 @@
 import styles from "./styles.module.css";
+import React, { useRef, useState } from "react";
 
 const Main = () => {
   const handleLogout = () => {
@@ -6,14 +7,60 @@ const Main = () => {
     window.location.reload();
   };
 
+  const inputRef = useRef(null);
+  const [img, setImg] = useState(null);
+
+  const handleImgclick = () => {
+    inputRef.current.click();
+  };
+  const handleImgchange = (event) => {
+    const file = event.target.files[0];
+    console.log(file);
+    setImg(event.target.files[0]);
+  };
+
   return (
     <div className={styles.main_container}>
       <nav className={styles.navbar}>
-        <h1>facebook</h1>
+        <h1>Image Verifier</h1>
         <button className={styles.white_btn} onClick={handleLogout}>
           Logout
         </button>
       </nav>
+      <div className={styles.imgupload_Container}>
+        <div className={styles.box_dec}>
+          <label
+            htmlFor="image-upload-input"
+            className={styles.image_upload_label}
+          >
+            {img ? img.name : "Choose an Image"}
+          </label>
+          <div onClick={handleImgclick} style={{ cursor: "pointer" }}>
+            {img ? (
+              <img
+                src={URL.createObjectURL(img)}
+                alt="new img"
+                className={styles.img_display_after}
+              />
+            ) : (
+              <img
+                src="images.png"
+                alt="uploadimg"
+                className={styles.img_display_before}
+              />
+            )}
+            <input
+              type="file"
+              ref={inputRef}
+              onChange={handleImgchange}
+              style={{ display: "none" }}
+            />
+          </div>
+          <button className={styles.img_upload_btn} type="submit">
+            upload
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
