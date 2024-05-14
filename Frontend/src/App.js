7c230631ef184f99e0e662cf8120eccc36ac519c
Vitharshanaasreek <99336479+Vitharshanaasreek@users.noreply.github.com>
2024-05-15 02:21:30 +0530

    email verify remove

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,7 +2,6 @@ import { Route, Routes, Navigate } from "react-router-dom";
 import Main from "./components/Main";
 import Signup from "./components/Signup";
 import Login from "./components/login";
-import EmailVerify from "./components/EmailVerify";
 
 
 function App() {
